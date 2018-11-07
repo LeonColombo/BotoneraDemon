63a158ba0c42185e8ed22707f802b71a7a13eb08
modicristian <cristianmodi@gmail.com>
2018-11-06 21:50:03 -0300

    si

--- app/src/main/java/com/example/itmaster/botonerdemon/MainActivity.java
+++ app/src/main/java/com/example/itmaster/botonerdemon/MainActivity.java
@@ -4,8 +4,8 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-import java.awt.Button;
 
 public class MainActivity extends AppCompatActivity
 {
